5e0001ca61398ce077b0eca8a7da9e34af0b8355
Olga Averianova <olgaolgar47@yandex.ru>
2024-01-17 01:46:25 -0300

    feat: Header

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,11 @@
 import { Routing } from '../pages/routing';
+import Header from '../components/Header/header';
 import styles from './app.module.scss';
 
 function App() {
 	return (
 		<>
+			<Header />
 			<div className={styles.main}>Hello I'm App</div>
 			<Routing />
 		</>
