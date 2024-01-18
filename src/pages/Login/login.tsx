d83c9d7ee32e8ab28694eabc97d9bbe105490e26
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-01-18 05:12:36 +0300

    Feat: rout-react-dom, routes in App, login page

---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './login.module.scss';
+import Header from '../../components/Header/header';
+import { Link, NavLink } from 'react-router-dom';
+import { users } from '../../shared/utils/users';
+
+function Login() {
+	return (
+		<div className={styles.container}>
+			<Header></Header>
+			<div className={styles.wrapper}>
+				<ul className={styles.list}>
+					<h6 className={styles.header}>Сотрудник</h6>
+
+					{users.map((user: any, key: any) => {
+						return (
+							<li className={styles.item} key={user.name}>
+								<img src={user.pic} className={styles.img} alt="аватар" />
+								<div className={styles.text_wrapper}>
+									<h3 className={styles.title}>{user.userName}</h3>
+									<p className={styles.paragraph}>{user.position}</p>
+								</div>
+								<NavLink to={user.link} className={styles.link} replace>
+									Войти
+								</NavLink>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
