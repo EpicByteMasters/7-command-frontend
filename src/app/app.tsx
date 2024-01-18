211f4a507d41df191b487fd4ae04f699cfd3c676
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-01-18 14:33:11 +0300

    feat: add rout to employee mentor page (rating)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,24 +1,19 @@
-import NavBar from '../entities/NavBar/navbar';
-import { Routing } from '../pages/routing';
-import Header from '../components/Header/header';
 import styles from './app.module.scss';
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login/login';
+import { EmployeeRatingPage } from '../pages/employee-rating/employee-rating';
 
 function App() {
 	return (
 		<div className={styles.container__main}>
-			{/* <Header />
-			<div className={styles.container}>
-				<NavBar />
-				<Routing />
-			</div> */}
-
 			<Routes>
 				<Route path="/alfa-hackathon-7-frontend" element={<Login />} />
 				<Route path="/alfa-hackathon-7-frontend/head" element={<></>} />
 				<Route path="/alfa-hackathon-7-frontend/employee" element={<></>} />
-				<Route path="/alfa-hackathon-7-frontend/mentor" element={<></>} />
+				<Route
+					path="/alfa-hackathon-7-frontend/mentor"
+					element={<EmployeeRatingPage />}
+				/>
 				<Route path="/*" element={<></>} />
 			</Routes>
 		</div>
--- src/pages/employee-rating/employee-rating.tsx
+++ src/pages/employee-rating/employee-rating.tsx
@@ -5,8 +5,10 @@ import { PickerButton } from '@alfalab/core-components/picker-button';
 import { Textarea } from '@alfalab/core-components/textarea';
 import { ChangeEvent, useState } from 'react';
 import avatar from '../../images/avatar.png';
+import Header from '../../components/Header/header';
+import NavBar from '../../entities/NavBar/navbar';
 
-export const EmployeeRatingComponent = () => {
+export const EmployeeRatingPage = () => {
 	const options = [{ key: 'Выполнен' }, { key: 'Не выполнен' }];
 
 	const [selectedRating, setSelectedRaiting] = useState<number>(0);
@@ -27,62 +29,68 @@ export const EmployeeRatingComponent = () => {
 	};
 
 	return (
-		<div className={styles.wrapper}>
-			<h2 className={styles.title}>План развития сотрудника</h2>
-			<div className={styles.employee__wrapper}>
-				<div className={styles.employee__card}>
-					<img src={avatar} alt="аватар" className={styles.avatar} />
-					<div className={styles.employee__data}>
-						<span className={styles.employee__name}>
-							Константинов Константин Игоревич
-						</span>
-						<span className={styles.employee__position}>
-							Фронтенд-разработчик
-						</span>
-					</div>
-				</div>
-			</div>
-
-			<div className={styles.btn__container}>
-				<PickerButton
-					options={options}
-					view="secondary"
-					label="Выбрать статус"
-				></PickerButton>
-				<Button view="primary" onClick={handleSubmit}>
-					Завершить
-				</Button>
-			</div>
-			<h3 className={styles.subtitle}>Оценка выполнения</h3>
-			<div className={styles.form__wrapper}>
-				<div className={styles.rating__wrapper}>
-					<div className={styles.rating}>
-						{Array.from({ length: 10 }, (_, index) => (
-							<div
-								key={index + 1}
-								onClick={() => handleRatingClick(index + 1)}
-								className={`${styles.rating__btn} ${
-									selectedRating >= index + 1 ? styles.clicked : ''
-								}`}
-							>
-								{index + 1}
+		<>
+			<Header />
+			<div className={styles.container}>
+				<NavBar />
+				<div className={styles.wrapper}>
+					<h2 className={styles.title}>План развития сотрудника</h2>
+					<div className={styles.employee__wrapper}>
+						<div className={styles.employee__card}>
+							<img src={avatar} alt="аватар" className={styles.avatar} />
+							<div className={styles.employee__data}>
+								<span className={styles.employee__name}>
+									Константинов Константин Игоревич
+								</span>
+								<span className={styles.employee__position}>
+									Фронтенд-разработчик
+								</span>
 							</div>
-						))}
+						</div>
 					</div>
-					<div className={styles.rating__span}>
-						<span>Есть над чем работать</span>
-						<span>Молодец!</span>
+
+					<div className={styles.btn__container}>
+						<PickerButton
+							options={options}
+							view="secondary"
+							label="Выбрать статус"
+						></PickerButton>
+						<Button view="primary" onClick={handleSubmit}>
+							Завершить
+						</Button>
+					</div>
+					<h3 className={styles.subtitle}>Оценка выполнения</h3>
+					<div className={styles.form__wrapper}>
+						<div className={styles.rating__wrapper}>
+							<div className={styles.rating}>
+								{Array.from({ length: 10 }, (_, index) => (
+									<div
+										key={index + 1}
+										onClick={() => handleRatingClick(index + 1)}
+										className={`${styles.rating__btn} ${
+											selectedRating >= index + 1 ? styles.clicked : ''
+										}`}
+									>
+										{index + 1}
+									</div>
+								))}
+							</div>
+							<div className={styles.rating__span}>
+								<span>Есть над чем работать</span>
+								<span>Молодец!</span>
+							</div>
+						</div>
+						<Textarea
+							label="Оцените достижение цели"
+							block={true}
+							minRows={3}
+							maxLength={96}
+							showCounter={true}
+							onChange={handleCommentChange}
+						/>
 					</div>
 				</div>
-				<Textarea
-					label="Оцените достижение цели"
-					block={true}
-					minRows={3}
-					maxLength={96}
-					showCounter={true}
-					onChange={handleCommentChange}
-				/>
 			</div>
-		</div>
+		</>
 	);
 };
