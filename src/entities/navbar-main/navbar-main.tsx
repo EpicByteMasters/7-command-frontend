90096a9417bee01f7a0e9c3263739bbfa6c522be
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-02-01 13:51:02 +0300

    fix: nav after main to myIprs

--- src/entities/navbar-main/navbar-main.tsx
+++ src/entities/navbar-main/navbar-main.tsx
@@ -1,7 +1,7 @@
 import styles from './navbar-main.module.scss';
 
-import { useAppSelector } from '../../shared/hooks/redux';
-import { NavLink, useNavigate, useLocation } from 'react-router-dom';
+//import { useAppSelector } from '../../shared/hooks/redux';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import iconProfile from '../../images/navbar-icons-color/icon-user-account.svg';
 import iconCert from '../../images/navbar-icons-color/icon-certificate.svg';
@@ -12,37 +12,41 @@ import iconMore from '../../images/navbar-icons-color/icon-more.svg';
 
 export const NavBarMain: React.FC = () => {
 	const navigate = useNavigate();
-	const location = useLocation();
+	// const location = useLocation();
 
-	const userData = useAppSelector((state) => state.user.user);
+	// const userData = useAppSelector((state) => state.user.user);
 
-	const isEmployee = userData.isSupervisor === false;
-	const isExecutive = userData.isSupervisor === true;
-	const isMentor = userData.isMentor === true;
+	// const isEmployee = userData.isSupervisor === false;
+	// const isExecutive = userData.isSupervisor === true;
+	// const isMentor = userData.isMentor === true;
 
-	const id = userData.id === 6;
-	const id2 = userData.id === 16;
+	// const id = userData.id === 6;
+	// const id2 = userData.id === 16;
 
 	// console.log('isExecutive Руководитель в Главном навбаре: ', isExecutive);
 	// console.log('isEmployee Работник в Главномнавбаре: ', isEmployee);
 	// console.log('isEmployee Ментор в Главном навбаре: ', isMentor);
 
-	const onNavigate = () => {
-		if (location.pathname === '/main' && isExecutive) {
-			navigate('/service-iprs/myteam', { replace: false });
-		}
-		if (location.pathname === '/main' && isEmployee) {
-			console.log('isEmployee', isEmployee);
-			navigate('/service-iprs/my', { replace: false });
-		}
-		if (location.pathname === '/main' && id) {
-			navigate('/service-iprs/mentor', { replace: true });
-		}
-		if (location.pathname === '/main' && id2) {
-			navigate('/service-iprs/mentor', { replace: true });
-		}
-	};
+	// const onNavigate = () => {
+	// 	if (location.pathname === '/main' && isExecutive) {
+	// 		navigate('/service-iprs/myteam', { replace: false });
+	// 	}
+	// 	if (location.pathname === '/main' && isEmployee) {
+	// 		console.log('isEmployee', isEmployee);
+	// 		navigate('/service-iprs/my', { replace: false });
+	// 	}
+	// 	if (location.pathname === '/main' && id) {
+	// 		navigate('/service-iprs/mentor', { replace: true });
+	// 	}
+	// 	if (location.pathname === '/main' && id2) {
+	// 		navigate('/service-iprs/mentor', { replace: true });
+	// 	}
+	// };
 	//TODO кода данные в базе будут ок убрать моки id
+
+	const onNavigate = () => {
+		navigate('/service-iprs/my');
+	};
 
 	return (
 		<aside className={styles.aside}>
