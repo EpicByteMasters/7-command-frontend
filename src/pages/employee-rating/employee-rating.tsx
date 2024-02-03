978ec05b500980ddec857d1d771d0857b091dafa
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-02-03 23:20:09 +0300

    feat: added opendIPR component

--- src/pages/employee-rating/employee-rating.tsx
+++ src/pages/employee-rating/employee-rating.tsx
@@ -1,25 +1,23 @@
 import styles from './employee-rating.module.scss';
+//--------------------------------------------------------------
+import { ChangeEvent, useState } from 'react';
+//--------------------------------------------------------------
 import { Button } from '@alfalab/core-components/button';
 import { PickerButton } from '@alfalab/core-components/picker-button';
-import { Textarea } from '@alfalab/core-components/textarea';
-import { ChangeEvent, useState } from 'react';
-import { EmployeeInfoCard } from '../../entities/employee-info-card/employee-info-card';
-import avatar from '../../images/avatar.png';
-import NavBarMini from '../../entities/navbar-mini/navbar-mini';
-import { FooterMain } from '../../entities/footer-main/footer-main';
-interface RatingProps {
-	isExecutive?: boolean;
+//--------------------------------------------------------------
+import { Raiting } from '../../shared/rating/rating';
+//--------------------------------------------------------------
+interface EmployeeRatingPickerProps {
+	withBtn?: boolean;
 }
 
-export const EmployeeRatingPage: React.FC<RatingProps> = ({ isExecutive }) => {
+export const EmployeeRatingPicker: React.FC<EmployeeRatingPickerProps> = ({
+	withBtn,
+}) => {
 	const options = [{ key: 'Выполнен' }, { key: 'Не выполнен' }];
 
 	const [selectedRating, setSelectedRaiting] = useState<number>(0);
 	const [comment, setComment] = useState<string>('');
-
-	const handleRatingClick = (rating: number) => {
-		setSelectedRaiting(rating);
-	};
 
 	const handleSubmit = () => {
 		console.log('Выбранная оценка:', selectedRating);
@@ -32,67 +30,22 @@ export const EmployeeRatingPage: React.FC<RatingProps> = ({ isExecutive }) => {
 	};
 
 	return (
-		<div className={styles.generalFooterWrapper}>
-			<div className={styles.generalFooterContainer}>
-				<div className={styles.container}>
-					<NavBarMini />
-
-					<div className={styles.wrapper}>
-						<h2 className={styles.title}>План развития сотрудника</h2>
-						<div className={styles.employeeWrapper}>
-							<EmployeeInfoCard
-								name="Константинов Константин Игоревич"
-								position="Фронтенд-разработчик"
-								avatar={avatar}
-							/>
-						</div>
-						<div className={styles.btnContainer}>
-							<PickerButton
-								options={options}
-								view="secondary"
-								label="Выбрать статус"
-							></PickerButton>
-							<Button view="primary" size="m" onClick={handleSubmit}>
-								Завершить
-							</Button>
-						</div>
-						<h3 className={styles.subtitle}>Оценка выполнения</h3>
-						<div className={styles.formWrapper}>
-							<div className={styles.ratingWrapper}>
-								<div className={styles.rating}>
-									{Array.from({ length: 10 }, (_, index) => (
-										<div
-											key={index + 1}
-											onClick={() => handleRatingClick(index + 1)}
-											className={`${styles.ratingBtn} ${
-												selectedRating >= index + 1 ? styles.clicked : ''
-											}`}
-										>
-											{index + 1}
-										</div>
-									))}
-								</div>
-								<div className={styles.ratingSpan}>
-									<span>Есть над чем работать</span>
-									<span>Молодец!</span>
-								</div>
-							</div>
-							<Textarea
-								label="Оцените достижение цели"
-								block={true}
-								minRows={3}
-								maxLength={96}
-								showCounter={true}
-								onChange={handleCommentChange}
-								allowOverflow={false}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className={styles.generalFooter}>
-					<FooterMain></FooterMain>
-				</div>
+		<div className={styles.wrapper}>
+			<div className={styles.btnContainer}>
+				{withBtn && (
+					<>
+						<PickerButton
+							options={options}
+							view="secondary"
+							label="Выбрать статус"
+						></PickerButton>
+						<Button view="primary" size="m" onClick={handleSubmit}>
+							Завершить
+						</Button>
+					</>
+				)}
 			</div>
+			<Raiting title="Оценка выполнения" />
 		</div>
 	);
 };
