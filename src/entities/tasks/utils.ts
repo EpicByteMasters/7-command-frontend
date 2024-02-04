49d312b5c630644dc1b2b22570300d0993791681
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-02-04 02:14:39 -0300

    feat: delete Task

--- src/entities/tasks/utils.ts
+++ src/entities/tasks/utils.ts
@@ -9,14 +9,18 @@ import { MONTH_FULL_NAME_LIST, PICKER_OPTIONS } from './const';
  */
 const getArrLastEl = <T>(arr: T[]): T | null => arr[arr.length - 1] || null;
 
-function formatDateToCustomFormat(dateString: any) {
-	console.log('dateString: ', dateString);
+function formatDateToCustomFormat(dateString?: string) {
+  console.log('dateString: ', dateString);
 
-	const [year, month, day] = dateString.split('-').map(Number);
+  if (!dateString) {
+    return '';
+  }
 
-	const formattedDate = `${day} ${MONTH_FULL_NAME_LIST[month - 1]}`;
+  const [year, month, day] = dateString.split('-').map(Number);
 
-	return `до ${formattedDate}`;
+  const formattedDate = `${day} ${MONTH_FULL_NAME_LIST[month - 1]}`;
+
+  return `до ${formattedDate}`;
 }
 
 // Local Course
@@ -26,28 +30,16 @@ function formatDateToCustomFormat(dateString: any) {
  * @param option - Опция курса или тренина
  * @param inputValues - Массив вводимых
  */
-const isCourseSelectedOption = (
-	option: ICoursesOption,
-	inputValues: string[]
-) => inputValues.includes(option.value.name);
+const isCourseSelectedOption = (option: ICoursesOption, inputValues: string[]) =>
+  inputValues.includes(option.value.name);
 
 /** Поиск опции, значение которой содержит подстроку с вводимым в поле ввода */
 function matchCourseOption(option: ICoursesOption, inputValue: string) {
-	return option.value.name.toLowerCase().includes(inputValue.toLowerCase());
+  return option.value.name.toLowerCase().includes(inputValue.toLowerCase());
 }
 
 /** Проверка на то что опция была выбрана */
-const isCourseFilteredOption = (
-	optionsSelected: ICoursesOption[],
-	option: ICoursesOption,
-	inputValue: string | null
-) =>
-	optionsSelected.includes(option) ||
-	(inputValue && matchCourseOption(option, inputValue));
+const isCourseFilteredOption = (optionsSelected: ICoursesOption[], option: ICoursesOption, inputValue: string | null) =>
+  optionsSelected.includes(option) || (inputValue && matchCourseOption(option, inputValue));
 
-export {
-	getArrLastEl,
-	isCourseSelectedOption,
-	isCourseFilteredOption,
-	formatDateToCustomFormat,
-};
+export { getArrLastEl, isCourseSelectedOption, isCourseFilteredOption, formatDateToCustomFormat };
